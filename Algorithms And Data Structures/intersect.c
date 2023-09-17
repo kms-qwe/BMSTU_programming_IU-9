59165e1c58358dae3c07738fce3e0237ab735b24
kms-qwe <mishakr8987@gmail.com>
2023-09-17 21:45:07 +0300

    second

---
+++ Algorithms And Data Structures/intersect.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a = 0, b = 0, len_x, xi, ans;
+    scanf("%d", &len_x);
+    for (int i = 0; i < len_x; i++)
+    {
+        scanf("%d", &xi);
+        a = a | (int)pow(2, xi);
+    }
+    scanf("%d", &len_x);
+    for (int i = 0; i < len_x; i ++)
+    {
+        scanf("%d", &xi);
+        b = b | (int)pow(2,xi);
+    }
+    ans = a & b;
+    for (int i = 0; i < 32; i++)
+    {   
+        if ((int)pow(2,i) == (ans & (int)pow(2,i)))
+        {
+            printf("%d\n", i); 
+        }
+    }
+}
