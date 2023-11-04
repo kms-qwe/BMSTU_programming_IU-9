8307dc921fa9d07e6b01bb86e043b4e9b397a74a
kms-qwe <145258857+kms-qwe@users.noreply.github.com>
2023-11-04 11:18:36 +0300

    Update 3_fibsys.c

--- Algorithms And Data Structures/Module 1/3_fibsys.c
+++ Algorithms And Data Structures/Module 1/3_fibsys.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-void fib_sys_print (long long x)
+void fib_sys_print (unsigned long x)
 {
+	if (x == 0)
+	{
+		printf("%d", 0);
+	}
     int ans[100] = {0};
     int mx_ind = 0;
     while (x > 0)
     {
-    long long fib_0[2] = {0, 1}, fib_1[2] = {1, 1}, fib_2[2] = {2, 2};
+    unsigned long long fib_0[2] = {0, 1}, fib_1[2] = {1, 1}, fib_2[2] = {2, 2};
         while (fib_2[1] <= x)
         {
             fib_0[1] = fib_1[1]; fib_0[0] += 1;
@@ -18,7 +22,7 @@ void fib_sys_print (long long x)
         x -= fib_1[1];
 
     }
-    for (int i = 1; i <= mx_ind; i++)
+    for (int i = mx_ind; i > 0; i--)
     {
         printf("%d", ans[i]);
     }
@@ -27,8 +31,8 @@ void fib_sys_print (long long x)
 
 int main()
 {
-    long long x;
-    scanf("%lld", &x);
+    unsigned long long x;
+    scanf("%llu", &x);
     fib_sys_print(x);
     return 0;
 }
