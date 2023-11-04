a3e6f0dfef39835407bfb694664639e47b494a54
kms-qwe <145258857+kms-qwe@users.noreply.github.com>
2023-11-04 11:37:42 +0300

    Update 8_saddlepoint.c

--- Algorithms And Data Structures/Module 1/8_saddlepoint.c
+++ Algorithms And Data Structures/Module 1/8_saddlepoint.c
@@ -10,26 +10,26 @@ int main(int argc, char const *argv[])
 {
 	int N, M;
 	scanf("%d%d", &N, &M);
-	int arr[10][10], min_in_row[N], max_in_colomn[M];
+	int arr[10][10], max_in_row[N], min_in_colomn[M];
 
 	for(int i = 0; i < N; i++)
 		for(int j = 0; j < M; j++)
 			scanf("%d", &arr[i][j]);
 
 	for(int i = 0; i < N; ++i)
-		min_in_row[i] = INT_MAX;
+		max_in_row[i] = INT_MIN;
 
 	for(int i = 0; i < M; i++)
-		max_in_colomn[i] = INT_MIN;
+		min_in_colomn[i] = INT_MAX;
 
 	for(int i = 0; i < N; ++i)
 		for(int j = 0; j < M; ++j)
 		{
-			min_in_row[i] = (min_in_row[i] > arr[i][j]) ? arr[i][j] : min_in_row[i];
-			max_in_colomn[j] = (max_in_colomn[j] < arr[i][j]) ? arr[i][j] : max_in_colomn[j];
+			max_in_row[i] = (max_in_row[i] < arr[i][j]) ? arr[i][j] : max_in_row[i];
+			min_in_colomn[j] = (min_in_colomn[j] > arr[i][j]) ? arr[i][j] : min_in_colomn[j];
 		}
 
-	any_in( max_in_colomn, min_in_row, N, M);
+	any_in( min_in_colomn, max_in_row, N, M);
 
 	return 0;
 }
