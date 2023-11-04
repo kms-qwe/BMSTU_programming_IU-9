8ae170b52343060efd39d2c3e72ef90da9ae55f5
kms-qwe <145258857+kms-qwe@users.noreply.github.com>
2023-11-04 10:44:02 +0300

    Update 2_mulmod.c

--- Algorithms And Data Structures/Module 1/2_mulmod.c
+++ Algorithms And Data Structures/Module 1/2_mulmod.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
-long long gorner_mod (long long a, long long b, long long m)
+unsigned long gorner_mod (unsigned long a, unsigned long b, unsigned long m)
 {
-    long long ans;
-    ans = (b / (long long)pow(2, 63)) * a % m;
-    b = b % (long long)pow(2,63);
+    unsigned long ans;
+
+    ans = (b / (unsigned long)pow(2, 63)) * a % m;
+    b = b % (unsigned long)pow(2,63);
     for (int i = 62; i >= 0; i--)
     {
-        ans = (ans % m * 2 % m) + (b / (long long)pow(2,i)) * a % m;
-        b = b % (long long)pow(2, i);
+        ans = ((ans * 2 % m) + (b / (unsigned long)pow(2,i)) * a % m) % m;
+        b = b % (unsigned long)pow(2, i);
     } 
     return ans;
 }
 
 int main()
 {
-    long long a, b, m;
-    scanf("%lld%lld%lld", &a, &b, &m);
-    printf("%lld", gorner_mod(a,b,m));
+    unsigned long a, b, m;
+    scanf("%lu%lu%lu", &a, &b, &m);
+    printf("%lu\n", gorner_mod(a,b,m));
     return 0;
 }
